fb2f09bff2b0329e505cd4ae8f6149979854d76f
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-10-16 22:56:51 +0200

    Error

--- frontend/src/routes/archivio/[slug]/+page.svelte
+++ frontend/src/routes/archivio/[slug]/+page.svelte
@@ -58,51 +58,7 @@
         }}
         />
       </div>
-      <!-- {#if data.archive[0]?.credits}
-        <div class="credits">
-          {#each data.archive[0]?.credits as credit, j}
-            {#if credit._type === 'partners' && credit.partnersArray?.length}
-              <p class="credit">Partners</p>
-              {#each credit.partnersArray as partner, k}
-                {#if partner.link}
-                  <a target="_blank" href={partner.link}><p>{partner.title}</p></a><br>
-                {:else}
-                  <p>{partner?.title}</p>
-                {/if}
-              {/each}
-            {:else if credit._type === 'sponsors' && credit.sponsorsArray?.length}
-              <p class="credit">Sponsors</p>
-              {#each credit.sponsorsArray as sponsor, k}
-                {#if sponsor.link}
-                  <a target="_blank" href={sponsor.link}><p>{sponsor.title}</p></a><br>
-                {:else}
-                  <p>{sponsor?.title}</p>
-                {/if}
-              {/each}
-            {:else if credit._type === 'patronages' && credit.patronagesArray?.length}
-              <p class="credit">Con il patrocinio di:</p>
-              {#each credit.patronagesArray as patronage, k}
-                {#if patronage.link}
-                  <a target="_blank" href={patronage.link}><p>{patronage.title}</p></a><br>
-                {:else}
-                  <p>{patronage?.title}</p>
-                {/if}
-              {/each}
-            {:else if credit._type === 'customCredit' && credit.customArray?.length}
-              <p class="credit">{credit.customText}</p>
-              {#each credit.customArray as custom, k}
-                {#if custom.link}
-                  <a target="_blank" href={custom.link}><p>{custom.title}</p></a><br>
-                {:else}
-                  <p>{custom?.title}</p>
-                {/if}
-              {/each}
-            {/if}
-          {/each}
-        </div>
-      {/if} -->
       {#if data.archive[0]?.credits}
-      {$inspect(data.archive[0]?.credits)}
         <div class="credits text-xs">
           {#each data.archive[0]?.credits as credit, j}
             {#if credit._type === 'partners' && credit.partnersArray?.length}
