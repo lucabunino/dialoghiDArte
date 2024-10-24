fca3065d13ce57012f236bfb54fe822d939bf818
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-10-24 12:26:15 +0200

    404

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<section class="single">
+  <h1 class="text-xl">404</h1>
+  <p>Questa pagina non esiste</p>
+  <a class="btn cta" href="/">Homepage →</a>
+</section>
+
+<style>
+.cta {
+  margin-top: calc(var(--margin)*3);
+}
+@media screen and (max-width: 900px) {
+  .cta {
+    margin-top: calc(var(--margin)*4.5);
+  }
+}
+</style>
