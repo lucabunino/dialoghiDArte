d07f2402c096b9b15c6aef3d369aedaaecb17ebe
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-10-25 12:52:10 +0200

    useCdn

--- frontend/src/lib/utils/sanity.js
+++ frontend/src/lib/utils/sanity.js
@@ -8,7 +8,7 @@ if (!PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET) {
 export const client = createClient({
 	projectId: PUBLIC_SANITY_PROJECT_ID,
 	dataset: PUBLIC_SANITY_DATASET,
-	useCdn: false, // `false` if you want to ensure fresh data
+	useCdn: true, // `false` if you want to ensure fresh data
 	apiVersion: '2024-09-23', // date of setup
 });
 
